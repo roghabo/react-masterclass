ea35ce45ee47159a9e3906175c390701b5eb961d
Jinyoung <roghabo@gmail.com>
2024-06-21 07:58:52 +0900

    finish todo project

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import ToDoList from "../ToDoList";
+import ToDoList from "../components/ToDoList";
 const router = createBrowserRouter([
   {
     path: "/",
